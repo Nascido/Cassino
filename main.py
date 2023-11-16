b170be814f9dbd5ad56520e67a8bd501b1cffd68
Rafael Nascimento <rafael124715@gmail.com>
2023-11-16 18:28:46 -0300

    Nova execução do objeto cassino

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
-from games import Player, Blackjack
+from games import Player
+from interface import Casino
 
 """
     Cassino Royal
@@ -11,4 +12,6 @@ player3 = Player("Carioca", 1000)
 
 players = [player1, player2, player3]
 
-blakjackGame = Blackjack(players, 5000)
+cassino = Casino(players)
+
+cassino.iniciarBlackjack()
