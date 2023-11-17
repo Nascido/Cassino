f02f804bbebb959b5fa8358c45cf15bab357ec5e
Rafael Nascimento <rafael124715@gmail.com>
2023-11-17 15:07:49 -0300

    Atualizando main

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
 
-from games import Player
-from interface import Casino
+from games import Player, Blackjack
 
 """
     Cassino Royal
 """
 
-player1 = Player("Rafael", 1000)
-player2 = Player("Éric", 1000)
-player3 = Player("Carioca", 1000)
+player1 = Player("Rafael", 1000, 1234)
+player2 = Player("Éric", 1000, 1234)
+player3 = Player("Carioca", 1000, 1234)
 
 players = [player1, player2, player3]
 
-cassino = Casino(players)
-
-cassino.iniciarBlackjack()
-
-cassino.show()
+game = Blackjack(players)
