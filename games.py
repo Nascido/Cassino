5513ba722881eefaa464f0389b72a19fde443cc6
Rafael Nascimento <rafael124715@gmail.com>
2023-11-17 15:07:21 -0300

    Criação da classe mãe Hand e métodos de execução paro jogo blackjack

--- games.py
+++ games.py
@@ -11,40 +11,29 @@ class Game:
         return self._players
 
 
-class Player:
-    def __init__(self, nome, cpf, senha, fichas=100):
-        self._name = nome
-        self._cpf = cpf
-        self._senha = senha
+class Hand:
+    def __init__(self):
         self._hand = []
-        self._fichas = fichas
         self.index = 0
 
-    # Métodos do Player
-    def comprarCarta(self, deck):
-        carta = deck.pop()
-        self._hand.append(carta)
+    def eraseHand(self):
+        self._hand = []
 
-    def testeFichas(self, valor):
-        return self._fichas > valor
-
-    def apostar(self, valor):
-        if self._fichas >= valor:
-            self._fichas -= valor
-            return valor
-        else:
-            raise ValueError
-
-    def receber(self, valor):
-        self._fichas += valor
-
-    def sum(self):
+    def sum21(self):
         soma = 0
         for carta in self._hand:
-            soma += int(carta)
+            if int(carta) == 11:
+                tmp = soma
+                soma += int(carta)
+                if soma > 21:
+                    soma = tmp + carta.shift()
+
+            else:
+                soma += int(carta)
 
         return soma
 
+    # Métodos típicos de uma lista
     def pop(self, indexCarta=0):
         return self._hand.pop(indexCarta)
 
@@ -60,6 +49,51 @@ class Player:
 
     def insert(self, index, item):
         self._hand.insert(index, item)
+
+    # Builtins
+    def __getitem__(self, item):
+        return self._hand[item]
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index < len(self._hand):
+            result = self._hand[self.index]
+            self.index += 1
+            return result
+        else:
+            self.index = 0
+            raise StopIteration
+
+    def __len__(self):
+        return len(self._hand)
+
+
+class Player(Hand):
+    def __init__(self, nome, cpf, senha, fichas=100):
+        super().__init__()
+        self._name = nome
+        self._cpf = cpf
+        self._senha = senha
+        self._fichas = fichas
+
+    # Métodos do Player
+    def comprarCarta(self, deck):
+        self._hand.append(deck.pop())
+
+    def testeFichas(self, valor):
+        return self._fichas > valor
+
+    def apostar(self, valor):
+        if self._fichas >= valor:
+            self._fichas -= valor
+            return valor
+        else:
+            raise ValueError
+
+    def receber(self, valor):
+        self._fichas += valor
 
     # Getters
     def getname(self):
@@ -77,54 +111,63 @@ class Player:
     def getplayerinfo(self):
         return self._name, self._cpf, self._senha, self._fichas
 
-    # Builtins
-    def __getitem__(self, item):
-        return self._hand[item]
+    def __str__(self):
+        return f"{self._name}: {self._hand}"
 
-    def __iter__(self):
-        return self
 
-    def __next__(self):
-        if self.index < len(self._hand):
-            result = self._hand[self.index]
-            self.index += 1
-            return result
+class Dealer(Hand):
+    def __init__(self, blackjack=False):
+        super().__init__()
+        self.gametipe = blackjack
+        self.auto = False
+        self.deck = None
+
+        if self.gametipe:
+            self.auto = True
+
+    def pegarCarta(self):
+        self._hand.append(self.deck.pop())
+
+    def iniciar(self, players):
+        self.deck = Deck(blackjack=self.gametipe)
+        self.deck.shuffle()
+
+        for player in players:
+            player.eraseHand()
+
+        if self.auto:
+            self.eraseHand()
+
+    def distribuir(self, players, handsize=2):
+        if self.deck is not None:
+            self.deck.distribuir(players, handsize)
+
+            if self.auto:
+                self._hand.append(self.deck.pop())
         else:
-            raise StopIteration
-
-    def __len__(self):
-        return len(self._hand)
-
-    def __int__(self):
-        return sum(self)
+            raise TypeError("Deck do dealer não inicializado!")
 
     def __str__(self):
-        return f"{self._name}"
+        return f"Dealer: {self._hand}"
 
 
 class Blackjack(Game):
-    def __init__(self, players, caixa, apostaInicial=20) -> None:
+    def __init__(self, players, caixa=10000, apostaInicial=20) -> None:
         super().__init__(players)
-        self._dealer = Player("Dealer", caixa, 1234)
-        self._deck = Deck(blackjack=True)
+        self._dealer = Dealer(blackjack=True)
         self._apostaInicial = apostaInicial
+        self._caixa = caixa
 
     def iniciar(self):
-        self._deck.shuffle()
-
-        self._dealer.append(self._deck.pop())
-
-        self._deck.distribuir(self._players, 2)
+        self._dealer.iniciar(self._players)
+        self._dealer.distribuir(self._players)
 
     def hit(self, player):
-        if int(player) < 21:
-            self._deck.givecard(player)
+        if player.sum21() < 21:
+            self._dealer.deck.givecard(player)
         else:
             raise ValueError("Valor 21 já alcançado ou estourado")
 
     def dealerTurn(self):
-        dealer = self._dealer
-        deck = self._deck
-
-        while int(dealer) < 17:
-            deck.givecard(dealer)
+        while self._dealer.sum21() < 17:
+            self._dealer.pegarCarta()
