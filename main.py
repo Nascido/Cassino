b54f03ab6927593f74152dbd3c2bb0189124caa3
Rafael Nascimento <rafael124715@gmail.com>
2023-11-28 23:27:52 -0300

    Acesso direto ao Blackjack para teste

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ from interface import Casino
 """
     Cassino Royal
 """
-players = []
+users = []
 
 with open('players.txt', 'r') as file:
     for line in file:
         nome, senha, fichas = line.strip().split(' - ')
         usr = Player(nome, senha, int(fichas))
-        players.append(usr)
+        users.append(usr)
 
-cassino = Casino(players)
-cassino.show()
+cassino = Casino(users)
+cassino.acess()
